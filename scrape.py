457d34b7d8651616378a56dd86b0328f9034e6fb
Sơn Nguyễn <son@sonng.dev>
2023-10-05 10:44:44 +0100

    feat: scrape all years

--- scrape.py
+++ scrape.py
@@ -1,19 +1,23 @@
 import scrapy
 import csv
+import os
 
 class TransferMarktSpider(scrapy.Spider):
     name = 'transfermarktspider'
 
-    season = 2022
+    season = 2018
     matchday = 1
-    start_urls = [f'https://www.transfermarkt.co.uk/premier-league/spieltagtabelle/wettbewerb/GB1?saison_id=2022&spieltag=1']
+    start_urls = [f'https://www.transfermarkt.co.uk/premier-league/spieltagtabelle/wettbewerb/GB1?saison_id={season}&spieltag={matchday}']
 
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
     }
 
     def parse(self, response):
-        with open(f'./output/season_{self.season}_matchday_{self.matchday}.csv', 'w') as file:
+        filename = f'./output/season_{self.season}/season_{self.season}_matchday_{self.matchday}.csv'
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        with open(filename, 'w') as file:
             writer = csv.writer(file)
 
             headers = ['#', 'Club', 'Matches', 'W', 'D', 'L', 'Goals', '+/-', 'Pts']
@@ -32,10 +36,17 @@ class TransferMarktSpider(scrapy.Spider):
                     data.append(field.css('::text').get().strip())
                 writer.writerow(data)
 
-            if self.matchday < 38:
-                self.matchday += 1
-                yield response.follow(self.get_url(), self.parse)
+            next_url = self.get_next_url()
+            if next_url:
+                yield response.follow(next_url, self.parse)
 
 
-    def get_url(self):
+    def get_next_url(self):
+        if self.matchday < 38:
+            self.matchday += 1
+        elif self.season < 2023:
+            self.season += 1
+            self.matchday = 1
+        else:
+            return None
         return f'https://www.transfermarkt.co.uk/premier-league/spieltagtabelle/wettbewerb/GB1?saison_id={self.season}&spieltag={self.matchday}'
